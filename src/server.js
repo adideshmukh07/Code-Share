e5637f6943937a8fea5b285d25498efa561ccc7d
Pavitra <pandeypavitra65@gmail.com>
2023-07-02 00:45:33 +0530

    update cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const env = require('dotenv').config()
 const connectDb = require('./config/dbconnection')
 const errorHandler = require("./middleware/errorhandler");
 
+app.use(cors())
 connectDb()
 const port = process.env.PORT || 5000
 
