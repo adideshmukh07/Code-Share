17c4711796b403f5a12462f3465eabbf25e2d45e
Vishal Ola <vishalola555@gmail.com>
2024-02-24 15:10:51 +0530

    SocketIO integrated

--- src/server.js
+++ src/server.js
@@ -1,19 +1,35 @@
 const cors = require('cors')
 const express = require('express')
 const app = express()
+const {Server} = require('socket.io');
 const env = require('dotenv').config()
 const connectDb = require('./config/dbconnection')
 const errorHandler = require("./middleware/errorhandler");
+const http = require('http');
 
+let server = http.createServer(app);
+let io = new Server(server,{
+    cors:{
+        origin:"*"
+    }
+});
+
+io.on('connection',(socket)=>{
+    console.log(socket.id);
+    // continue from here 
+    socket.on('code-change',(newCode)=>{
+        socket.broadcast.emit('code-update',newCode);
+    })
+})
 app.use(cors())
 connectDb()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 app.use(errorHandler);
 app.use(express.json())
 app.use('/', require('./routes/pasteroutes'))
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
